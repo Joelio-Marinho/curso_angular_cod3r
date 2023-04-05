0ad56014f71920cf63be647f1534698bc141cbf8
Joélio Marinho <77017058+Joelio-Marinho@users.noreply.github.com>
2023-04-05 16:55:00 -0300

    adição de novos componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,15 +8,21 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+
 import { MatSidenavModule } from'@angular/material/sidenav'
-import {MatListModule} from '@angular/material/list'
+import { MatCardModule } from'@angular/material/card'
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +30,8 @@ import {MatListModule} from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
