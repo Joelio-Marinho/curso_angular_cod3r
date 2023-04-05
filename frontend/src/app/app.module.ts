87e90fa7d358604077d5703f308ecd038ac098be
Joélio Marinho <77017058+Joelio-Marinho@users.noreply.github.com>
2023-04-05 17:44:28 -0300

    adicao de diretivas e criacao e components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,10 @@ import { MatSidenavModule } from'@angular/material/sidenav'
 import { MatCardModule } from'@angular/material/card'
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product-create/product-create.component'
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
