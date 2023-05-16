e41c89d5d7525446a5aad7760690e2b9a47e2c1c
Joélio Marinho <77017058+Joelio-Marinho@users.noreply.github.com>
2023-05-16 15:26:30 -0300

    finalização do curso de angular

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,9 +1,10 @@
+import { catchError, map } from 'rxjs/operators';
 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,16 +16,24 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMenssage(msg: string): void {
+  showMenssage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      verticalPosition: "top",
+      panelClass: isError ? ["msg-error] : ['msg-success']
+    });
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorHandle(e))
+    )
+  }
+  errorHandle(e: any): Observable<any> {
+    this.showMenssage('ocorreu um erro!',true);
+    return EMPTY
   }
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HeaderData } from './../../components/template/header/header-data-model';
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerService: HeaderService) {
+    headerService.HeaderData = {
+      title:'inicio',
+      icon:'home',
+      routeUrl:''
+    }
+   }
 
   ngOnInit(): void {
   }
